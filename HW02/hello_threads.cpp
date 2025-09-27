8db495ebfb868182bf15c47a5d2a1a805f999e18
tcyang4 <tcyang4@wisc.edu>
2025-09-26 23:23:13 -0500

    Update hello_threads.cpp

--- HW02/hello_threads.cpp
+++ HW02/hello_threads.cpp
@@ -5,11 +5,11 @@ void hello(intid,inttotal){
 std::cout<<"Hellofromthread"<<id<<"of"<<total<<"\n";
 }
 int main() {
-constintN=5;
+const int N=5;
 std::vector<std::thread>threads;
 threads.reserve(N);
 for(int i=0;i<N;++i)
 threads.emplace_back(hello,i,N);
 for(auto &t:threads)t.join();
-return0;
+return 0;
 }
